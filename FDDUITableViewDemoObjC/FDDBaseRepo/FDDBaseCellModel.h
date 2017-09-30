5bd2a7f6d2933a5ff232c1ff7dd715142af9e6d1
Harry <328418417@qq.com>
2017-09-29 22:45:36 -0500

    Update FDDBaseCellModel.h

--- FDDUITableViewDemoObjC/FDDBaseRepo/FDDBaseCellModel.h
+++ FDDUITableViewDemoObjC/FDDBaseRepo/FDDBaseCellModel.h
@@ -12,6 +12,8 @@
 @interface FDDBaseCellModel : NSObject
 
 @property (nonatomic, strong) id cellData;                      //cell的数据源
+
+//https://stackoverflow.com/questions/8592289/arc-the-meaning-of-unsafe-unretained
 @property (nonatomic, assign) Class cellClass;                  //cell的Class
 @property (nonatomic, weak)   id delegate;                      //cell的代理
 @property (nonatomic, assign) CGFloat cellHeight;               //cell的高度，提前计算好
